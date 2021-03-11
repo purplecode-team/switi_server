b1e199af3560d2d949fddf6400ee9821602d51ba
shshshee <sehhe22@naver.com>
2021-03-11 23:36:19 +0900

    [Feat] 로그인 기능 구현

---
+++ routes/middlewares.js
@@ -0,0 +1,12 @@
+const jwt = require('jsonwebtoken');
+
+// 로그인 jwt 토큰 검사
+exports.isLoggedIn = (req,res,next) => {
+    try{
+        req.decoded = jwt.verify(req.session.jwt, process.env.JWT_SECRET);
+        return next();
+    }catch(err){
+        console.error(err);
+        return res.status(401).send({message:'유효하지 않는 토큰입니다.'});
+    }
+};
