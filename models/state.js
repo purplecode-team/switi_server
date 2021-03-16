a0e0fb2160e6f98e7ce206ad8d7d641888ce0938
shshshee <sehhe22@naver.com>
2021-03-17 00:08:28 +0900

    [Feat] 프로필 설정기능 구현

--- models/state.js
+++ models/state.js
@@ -10,8 +10,8 @@ module.exports = class State extends Sequelize.Model {
         }, {
             sequelize,
             timestamps:false,
-            modelName:'Interest',
-            tableName:'Interest',
+            modelName:'State',
+            tableName:'State',
             paranoid:false,
             charset:'utf8mb4',
             collate:'utf8mb4_general_ci',
--- routes/user.js
+++ routes/user.js
@@ -5,9 +5,29 @@ const { isLoggedIn } = require('./middlewares');
 const router = express.Router();
 
 // 프로필 설정
-router.put('/setProfile',async(req,res)=>{
+router.put('/setProfile',isLoggedIn,async(req,res)=>{
    // 나이, 관심지역, 관심분야, 성격, 현재 상황, 자기소개
+    const userId = req.decoded.id;
+    const { age, myRegion, myInterest, myCharacter, myState ,aboutme } = req.body;
 
+    try{
+        const user = await User.findOne({where:{id:userId}});
+        const result = await user.update({
+            age:age,
+            aboutme:aboutme,
+        });
+        if(result){
+            await user.addState(myState);
+            await user.addCharacter(myCharacter);
+            await user.addInterest(myInterest);
+            await user.addGu(myRegion);
+            return res.status(200).send({result:true});
+        }
+        return res.status(500).send({result:false});
+    }catch(err){
+        console.error(err);
+        return res.status(500).send({result:false});
+    }
 });
 
 module.exports = router;
