ad380ddb483581382a8483ce16e33125472306b2
shshshee <sehhe22@naver.com>
2021-03-28 19:59:43 +0900

    [Feat] 회원정보 수정

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const bcrypt = require('bcrypt');
 const { Study,Image,sequelize,Interest,Gu,State,User } = require('../models');
 const { isLoggedIn } = require('./middlewares');
 const path = require('path');
@@ -32,5 +33,42 @@ router.get('/mypage/:id',isLoggedIn,async(req,res)=>{
     }
 
 });
+
+//회원정보 수정
+router.put('/updateUserinfo',isLoggedIn,async(req,res)=>{
+
+    const id = req.decoded.id; // 유저 id 값 가져오기
+    const {nickname,email,newPassword} = req.body;
+    let {password} = req.body;
+
+    try{
+        const user = await User.findOne({where:{id}});
+        // new password 가 입력되었을 경우
+        if(newPassword) {
+            // 기존 비밀번호 비교
+            const result = await bcrypt.compare(password,user.password);
+            if(result){
+                // 기존 비밀번호 일치 시
+                password = await bcrypt.hash(newPassword, 12);
+                console.log(password);
+            }else{
+                return res.status(404).send({result:false,message:"비밀번호 불일치"});
+            }
+        }
+
+        await user.update({
+            email,
+            nickname,
+            password
+        })
+
+        return res.status(200).send({result:true});
+
+
+    }catch(err){
+        console.error(err);
+        return res.status(500).send({result:false});
+    }
+})
 
 module.exports = router;
