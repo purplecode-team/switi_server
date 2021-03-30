42beeade701c1bab263cb02d4a7a814210688829
shshshee <sehhe22@naver.com>
2021-03-30 22:35:18 +0900

    [Fix] 스터디 목록 지역 추가

--- routes/study.js
+++ routes/study.js
@@ -1,31 +1,8 @@
 const express = require('express');
-const multer = require('multer');
-const { Study,Image,sequelize,Interest,Gu,State,User } = require('../models');
+const { Study,Image,sequelize,Interest,Gu,State,User,Region } = require('../models');
 const { isLoggedIn } = require('./middlewares');
-const path = require('path');
-const fs = require('fs');
-
+const upload = require('./multer');
 const router = express.Router();
-
-try {
-    fs.readdirSync('uploads');
-} catch (error) {
-    console.error('uploads 폴더 생성');
-    fs.mkdirSync('uploads');
-}
-
-const upload = multer({
-    storage: multer.diskStorage({
-        destination(req,file,cb) {
-            cb(null,'uploads/');
-        },
-        filename(req,file,cb) {
-            const ext = path.extname(file.originalname); //확장자
-            cb(null,path.basename(file.originalname, ext) + Date.now() + ext);
-        },
-    }),
-    limits: {fileSize: 5 * 1024 * 1024},
-});
 
 //스터디 작성
 router.post('/addStudy',isLoggedIn,upload.single('img'),async(req,res)=>{
@@ -33,7 +10,7 @@ router.post('/addStudy',isLoggedIn,upload.single('img'),async(req,res)=>{
     console.log(req.file);
     const imgPath = req.file.filename;
     //카테고리(3개 이하) , 지역(3개 이하), 모집대상 , 모집인원, 모임장소, 활동기간, 예정종료일, 문의연락처, 제목 ,내용 , 이미지사진
-    const {online_flag, category, address, target, recruit_num, detail_address, period, endDate, contact, title, desc } = req.body;
+    const {online_flag, category, address, recruit_num, detail_address, period, endDate, contact, title, desc } = req.body;
     const t = await sequelize.transaction(); // 트랜잭션 생성
     //console.log(req.body);
 
@@ -81,16 +58,16 @@ router.get('/studyList/:onlineFlag',isLoggedIn,async(req,res)=>{
     try{
         const study = await Study.findAll({
             attributes:{include: [
-                [
-                    sequelize.literal(`(
+                    [
+                        sequelize.literal(`(
                     SELECT COUNT(*)
                     FROM likedList
                     WHERE
                     Study.id = likedList.StudyId
                     )`),
-                    'scrapCount'
-                ]
-            ]},
+                        'scrapCount'
+                    ]
+                ]},
             include:[{
                 model:Interest,
                 attributes:['category']
@@ -100,6 +77,13 @@ router.get('/studyList/:onlineFlag',isLoggedIn,async(req,res)=>{
             }, {
                 model:State,
                 attributes:['category']
+            },{
+                model:Gu,
+                attributes:['gu'],
+                include:[{
+                    model:Region,
+                    attributes:['city']
+                }]
             }], where: {online_flag: flag}
         });
         return res.status(200).send({result:true,study});
